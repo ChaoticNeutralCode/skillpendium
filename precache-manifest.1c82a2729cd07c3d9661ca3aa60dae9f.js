e786b9fc798f71281ba184707052f6629e450a10
Richard Key <rich@busyrich.com>
2020-07-11 11:00:29 -0700

    Updates

---
+++ precache-manifest.1c82a2729cd07c3d9661ca3aa60dae9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce139618d7b014708c896b4e7a40fa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a9a111007cdfc99338",
+    "url": "/static/css/main.7b2e183f.chunk.css"
+  },
+  {
+    "revision": "384d60583748a8a00679",
+    "url": "/static/js/2.e12b4b0b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e12b4b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a9a111007cdfc99338",
+    "url": "/static/js/main.6f7cf316.chunk.js"
+  },
+  {
+    "revision": "c180466bb0cdefaca483",
+    "url": "/static/js/runtime-main.792db947.js"
+  }
+]);
